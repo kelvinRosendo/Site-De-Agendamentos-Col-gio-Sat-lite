e9c88ef25c262ecfbe58202024b08af86b377336
Kelvin-Rosendo <rosendokelvin8@gmail.com>
2025-10-16 14:41:20 -0300

    refactor: update Next.js configuration to use turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.infrastructureLogging = { level: 'error' }; // Remove logs
-    config.stats = 'errors-warnings';
-    return config;
+  turbopack: {
+    root: path.resolve(__dirname),
   },
 };
 
